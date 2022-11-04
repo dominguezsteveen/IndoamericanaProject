5c1bfe360730c1241013afdc1de2629fcb796b83
SteveenDominguez <sdominguez@indoamericana.edu.co>
2022-11-04 16:36:58 -0500

    cambiando dimension y extension a las imagenes

--- database/seeders/WebImgSliderSeeder.php
+++ database/seeders/WebImgSliderSeeder.php
@@ -16,42 +16,42 @@ class WebImgSliderSeeder extends Seeder
     public function run()
     {
         $img = new WebImgSlider();
-        $img->urlImg = "images/web/inicio/slider/BANNER-UNO2b-1.png";
+        $img->urlImg = "images/web/inicio/slider/BANNER-UNO2b-1.webp";
         $img->urlRedirect = "/";
         $img->position = 1;
         $img->save();
 
         $img = new WebImgSlider();
-        $img->urlImg = "images/web/inicio/slider/examenes-practicos-2-01.jpg";
+        $img->urlImg = "images/web/inicio/slider/examenes-practicos-2-01.webp";
         $img->urlRedirect = "/";
         $img->position = 2;
         $img->save();
 
         $img = new WebImgSlider();
-        $img->urlImg = "images/web/inicio/slider/examenes-practicos-2-fuerzas-01-1.jpg";
+        $img->urlImg = "images/web/inicio/slider/examenes-practicos-2-fuerzas-01-1.webp";
         $img->urlRedirect = "/";
         $img->position = 3;
         $img->save();
 
         $img = new WebImgSlider();
-        $img->urlImg = "images/web/inicio/slider/Artes-cursos-continuada-06.jpg";
+        $img->urlImg = "images/web/inicio/slider/Artes-cursos-continuada-06.webp";
         $img->urlRedirect = "/";
         $img->position = 4;
         $img->save();
 
         $img = new WebImgSlider();
-        $img->urlImg = "images/web/inicio/slider/820x312-CINCO-1.png";
+        $img->urlImg = "images/web/inicio/slider/820x312-CINCO-1.webp";
         $img->urlRedirect = "/";
         $img->position = 5;
         $img->save();
 
         $img = new WebImgSlider();
-        $img->urlImg = "images/web/inicio/slider/no-se-deje-engañar-03.jpg";
+        $img->urlImg = "images/web/inicio/slider/no-se-deje-engañar-03.webp";
         $img->urlRedirect = "/";
         $img->save();
 
         $img = new WebImgSlider();
-        $img->urlImg = "images/web/inicio/slider/Slide-Ponal-1.jpg";
+        $img->urlImg = "images/web/inicio/slider/Slide-Ponal-1.webp";
         $img->urlRedirect = "/";
         $img->save();
     }
